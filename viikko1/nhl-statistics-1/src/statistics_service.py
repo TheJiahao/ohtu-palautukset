ea7a1f6ee4e8e59d9acc1e9fb258d9ea192b66bd
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-01 17:44:50 +0200

    Lisää v1t17

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,8 +1,24 @@
+from enum import Enum
+
 from player_reader import PlayerReader
 
 
 def sort_by_points(player):
     return player.points
+
+
+def sort_by_goals(player):
+    return player.goals
+
+
+def sort_by_assists(player):
+    return player.assists
+
+
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 
 class StatisticsService:
@@ -19,18 +35,19 @@ class StatisticsService:
         return None
 
     def team(self, team_name):
-        players_of_team = filter(
-            lambda player: player.team == team_name,
-            self._players
-        )
+        players_of_team = filter(lambda player: player.team == team_name, self._players)
 
         return list(players_of_team)
 
-    def top(self, how_many):
+    def top(self, how_many, sort_by=SortBy.POINTS):
+        sorting_method = {
+            1: sort_by_points,
+            2: sort_by_goals,
+            3: sort_by_assists,
+        }
+
         sorted_players = sorted(
-            self._players,
-            reverse=True,
-            key=sort_by_points
+            self._players, reverse=True, key=sorting_method[sort_by.value]
         )
 
         result = []
--- viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
+++ viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
@@ -1,23 +1,24 @@
 import unittest
-from statistics_service import StatisticsService
+
 from player import Player
+from statistics_service import SortBy, StatisticsService
+
 
 class PlayerReaderStub:
     def get_players(self):
         return [
             Player("Semenko", "EDM", 4, 12),
             Player("Lemieux", "PIT", 45, 54),
-            Player("Kurri",   "EDM", 37, 53),
+            Player("Kurri", "EDM", 37, 53),
             Player("Yzerman", "DET", 42, 56),
-            Player("Gretzky", "EDM", 35, 89)
+            Player("Gretzky", "EDM", 35, 89),
         ]
+
 
 class TestStatisticsService(unittest.TestCase):
     def setUp(self):
         # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
-        self.stats = StatisticsService(
-            PlayerReaderStub()
-        )
+        self.stats = StatisticsService(PlayerReaderStub())
 
     def test_search(self):
         self.assertEqual(self.stats.search("Lemieux"), Player("Lemieux", "PIT", 45, 54))
@@ -31,13 +32,16 @@ class TestStatisticsService(unittest.TestCase):
         self.assertEqual(len(team), 3)
 
         self.assertIn(Player("Semenko", "EDM", 4, 12), team)
-        self.assertIn(Player("Kurri",   "EDM", 37, 53), team)
+        self.assertIn(Player("Kurri", "EDM", 37, 53), team)
         self.assertIn(Player("Gretzky", "EDM", 35, 89), team)
 
     def test_team_with_no_players(self):
         self.assertEqual(self.stats.team("A"), [])
 
-    def test_top(self):
+    def test_top_return_empty_list_with_zero(self):
+        self.assertEqual(len(self.stats.top(0)), 0)
+
+    def test_top_without_sort_by(self):
         top3 = self.stats.top(3)
 
         self.assertEqual(len(top3), 3)
@@ -46,5 +50,29 @@ class TestStatisticsService(unittest.TestCase):
         self.assertEqual(top3[1], Player("Lemieux", "PIT", 45, 54))
         self.assertEqual(top3[2], Player("Yzerman", "DET", 42, 56))
 
-    def test_top_return_empty_list_with_zero(self):
-        self.assertEqual(len(self.stats.top(0)), 0)
+    def test_top_by_points(self):
+        top3 = self.stats.top(3)
+
+        self.assertEqual(len(top3), 3)
+
+        self.assertEqual(top3[0], Player("Gretzky", "EDM", 35, 89))
+        self.assertEqual(top3[1], Player("Lemieux", "PIT", 45, 54))
+        self.assertEqual(top3[2], Player("Yzerman", "DET", 42, 56))
+
+    def test_top_by_goals(self):
+        top3 = self.stats.top(3, SortBy.GOALS)
+
+        self.assertEqual(len(top3), 3)
+
+        self.assertEqual(top3[0], Player("Lemieux", "PIT", 45, 54))
+        self.assertEqual(top3[1], Player("Yzerman", "DET", 42, 56))
+        self.assertEqual(top3[2], Player("Kurri", "EDM", 37, 53))
+
+    def test_top_by_assists(self):
+        top3 = self.stats.top(3, SortBy.ASSISTS)
+
+        self.assertEqual(len(top3), 3)
+
+        self.assertEqual(top3[0], Player("Gretzky", "EDM", 35, 89))
+        self.assertEqual(top3[1], Player("Yzerman", "DET", 42, 56))
+        self.assertEqual(top3[2], Player("Lemieux", "PIT", 45, 54))
