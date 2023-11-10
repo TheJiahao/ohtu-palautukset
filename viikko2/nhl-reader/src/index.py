6801f1c218c86fb38b2b407e6621d5751409d466
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-10 10:04:48 +0200

    Poista JSON-tulostus

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -6,9 +6,6 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
