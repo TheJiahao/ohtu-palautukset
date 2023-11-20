b98ceb39f471da042b3ce3d450c0f1c08941369b
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-20 14:44:14 +0200

    Lisää v4t2

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -11,7 +11,7 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 10
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_called_with(HINTA)
@@ -19,7 +19,21 @@ class TestKassapaate(unittest.TestCase):
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
         maksukortti_mock = Mock()
         maksukortti_mock.saldo = 4
-        
+
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kortille_voi_ladata_jos_summa_on_positiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, 1)
+
+        maksukortti_mock.lataa.assert_called()
+
+    def test_kortille_voi_ladata_jos_summa_on_negatiivinen(self):
+        maksukortti_mock = Mock()
+
+        self.kassa.lataa(maksukortti_mock, -1)
+
+        maksukortti_mock.lataa.assert_not_called()
