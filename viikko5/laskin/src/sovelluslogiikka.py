10417e5f8a928443cc4e29a0af042eecb7586e04
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-28 14:32:54 +0200

    Alusta v5t4

---
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -0,0 +1,18 @@
+class Sovelluslogiikka:
+    def __init__(self, arvo: int = 0):
+        self._arvo: int = arvo
+
+    def miinus(self, operandi):
+        self._arvo = self._arvo - operandi
+
+    def plus(self, operandi):
+        self._arvo = self._arvo + operandi
+
+    def nollaa(self):
+        self._arvo = 0
+
+    def aseta_arvo(self, arvo):
+        self._arvo = arvo
+
+    def arvo(self):
+        return self._arvo
