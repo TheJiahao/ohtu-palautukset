156533242aa98a7bad198c5c4643c89f7710f38c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-05 15:10:25 +0200

    Lisää All, Not ja HasFewerThan

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,8 +1,8 @@
 from player import Player
 from abc import abstractmethod
 
-class Matcher:
 
+class Matcher:
     @abstractmethod
     def test(self, player: Player) -> bool:
         pass
@@ -37,3 +37,26 @@ class HasAtLeast(Matcher):
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All(Matcher):
+    def test(self, player: Player) -> bool:
+        return True
+
+
+class Not(Matcher):
+    def __init__(self, matcher: Matcher) -> None:
+        self.__matcher: Matcher = matcher
+
+    def test(self, player: Player) -> bool:
+        return not self.__matcher.test(player)
+
+
+class HasFewerThan(Matcher):
+    def __init__(self, value: int, attr) -> None:
+        self.__value: int = value
+        self.__attr: str = attr
+        self.__matcher: Matcher = Not(HasAtLeast(self.__value, self.__attr))
+
+    def test(self, player: Player) -> bool:
+        return self.__matcher.test(player)
