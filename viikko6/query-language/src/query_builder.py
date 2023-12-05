c9a6ea82788772a5d5ce748d48c08ca89659507d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-05 15:32:39 +0200

    Lisää v6t4

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,18 @@
+from matchers import *
+
+
+class QueryBuilder:
+    def __init__(self, matcher: Matcher = All()) -> None:
+        self.__matcher: Matcher = matcher
+
+    def playsIn(self, team: str) -> "QueryBuilder":
+        return QueryBuilder(And(self.__matcher, PlaysIn(team)))
+
+    def hasAtLeast(self, value: int, attr: str) -> "QueryBuilder":
+        return QueryBuilder(And(self.__matcher, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value: int, attr: str) -> "QueryBuilder":
+        return QueryBuilder(And(self.__matcher, HasFewerThan(value, attr)))
+
+    def build(self) -> Matcher:
+        return self.__matcher
