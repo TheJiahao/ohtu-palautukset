6cf42f71463df18a3968460477383649116937d6
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-15 14:42:17 +0200

    Refaktoroi ui

--- viikko7/kivi-paperi-sakset/src/ui/ui.py
+++ viikko7/kivi-paperi-sakset/src/ui/ui.py
@@ -1,42 +1,48 @@
 from services.logiikka import Logiikka
+from typing import Callable
 
 
 class UI:
     def __init__(self, logiikka: Logiikka | None = None) -> None:
         self.__logiikka: Logiikka = logiikka or Logiikka()
 
-    def kaynnista() -> None:
+        self.__ohje: str = "\n".join(
+            [
+                "Valitse pelataanko",
+                f"{'kaksinpeli:':15} Ihmistä vastaan",
+                f"{'helppo:':15} Tekoälyä vastaan",
+                f"{'vaikea:':15} Parannettua tekoälyä vastaan",
+                "Muilla valinnoilla lopetetaan",
+            ]
+        )
+
+        self.__pelit: dict[str, Callable] = {"kaksinpeli": self.kaynnista_kaksinpeli}
+
+    def kaynnista(self) -> None:
         while True:
-            print(
-                "Valitse pelataanko"
-                "\n (a) Ihmistä vastaan"
-                "\n (b) Tekoälyä vastaan"
-                "\n (c) Parannettua tekoälyä vastaan"
-                "\nMuilla valinnoilla lopetetaan"
-            )
+            self.tulosta_ohje()
 
             vastaus = input()
 
-            if vastaus.endswith("a"):
+            if vastaus in self.__pelit:
                 print(
                     "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
                 )
 
-                kaksinpeli = KPSPelaajaVsPelaaja()
-                kaksinpeli.pelaa()
-            elif vastaus.endswith("b"):
-                print(
-                    "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-                )
+                self.__logiikka.aseta_peli(vastaus)
+                self.__pelit[vastaus]()
 
-                yksinpeli = KPSTekoaly()
-                yksinpeli.pelaa()
-            elif vastaus.endswith("c"):
-                print(
-                    "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-                )
+            break
 
-                haastava_yksinpeli = KPSParempiTekoaly()
-                haastava_yksinpeli.pelaa()
-            else:
-                break
+    def tulosta_ohje(self) -> None:
+        print(self.__ohje)
+
+    def kaynnista_kaksinpeli(self) -> None:
+        while True:
+            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+            tokan_siirto = input("Toisen pelaajan siirto: ")
+
+            self.__logiikka.anna_pelaaja1().aseta_siirto(ekan_siirto)
+            self.__logiikka.anna_pelaaja2().aseta_siirto(tokan_siirto)
+
+            self.__logiikka.pelaa()
