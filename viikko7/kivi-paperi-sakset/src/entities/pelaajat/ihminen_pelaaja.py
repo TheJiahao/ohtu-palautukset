a172ded0437eefed824b92393881e1230f90ae58
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-15 14:07:41 +0200

    Refaktoroi IhminenPelaaja

--- viikko7/kivi-paperi-sakset/src/entities/pelaajat/ihminen_pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/entities/pelaajat/ihminen_pelaaja.py
@@ -1,5 +1,13 @@
 from entities.pelaajat.pelaaja import Pelaaja
 
+
 class IhminenPelaaja(Pelaaja):
+    def __init__(self, nimi: str) -> None:
+        self.__siirto: str = "k"
+        super().__init__(nimi)
+
     def anna_siirto(self) -> str:
-        return input(f"Syötä pelaajan {self.nimi} siirto: ")
+        return self.__siirto
+
+    def aseta_siirto(self, siirto: str) -> None:
+        self.__siirto = siirto
