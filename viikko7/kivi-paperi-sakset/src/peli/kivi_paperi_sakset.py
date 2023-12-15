0ce2d09a76022b50395ea02eab2b4d432e9f6f61
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-15 13:33:52 +0200

    Refaktoroi pelaa

--- viikko7/kivi-paperi-sakset/src/peli/kivi_paperi_sakset.py
+++ viikko7/kivi-paperi-sakset/src/peli/kivi_paperi_sakset.py
@@ -4,15 +4,20 @@ from entities.pelaaja import Pelaaja
 from entities.tuomari import Tuomari
 
 
-class KiviPaperiSakset(ABC):
+class KiviPaperiSakset:
     def __init__(self, pelaaja1: Pelaaja, pelaaja2: Pelaaja) -> None:
         self._pelaaja1: Pelaaja = pelaaja1
         self._pelaaja2: Pelaaja = pelaaja2
         self._tuomari: Tuomari = Tuomari()
 
-    @abstractmethod
     def pelaa(self) -> str:
-        pass
+        siirto1 = self._pelaaja1.anna_siirto()
+        siirto2 = self._pelaaja2.anna_siirto()
+
+        while self._tarkista_siirto(siirto1) and self._tarkista_siirto(siirto2):
+            self._tuomari.kirjaa_siirto(siirto1, siirto2)
+
+        return str(self._tuomari)
 
     def _tarkista_siirto(self, siirto: str) -> bool:
         return siirto in {"k", "p", "s"}
