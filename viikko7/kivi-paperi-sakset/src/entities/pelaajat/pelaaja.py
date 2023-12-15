47ed2bc25c6ec6e4ef19d8c61e3a437f25a2da85
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-15 13:45:03 +0200

    Lisää nimi Pelaaja-olioille

--- viikko7/kivi-paperi-sakset/src/entities/pelaajat/pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/entities/pelaajat/pelaaja.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Pelaaja(ABC):
+    def __init__(self, nimi: str) -> str:
+        self.nimi: str = nimi
+
     @abstractmethod
     def anna_siirto(self) -> str:
         pass
