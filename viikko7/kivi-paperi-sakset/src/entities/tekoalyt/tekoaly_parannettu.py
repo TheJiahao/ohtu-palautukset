0538ca522b333f443c651cd73a0919a038ddb1de
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-12-16 14:59:29 +0200

    Refaktoroi TekoalyParannettu

--- viikko7/kivi-paperi-sakset/src/entities/tekoalyt/tekoaly_parannettu.py
+++ viikko7/kivi-paperi-sakset/src/entities/tekoalyt/tekoaly_parannettu.py
@@ -6,54 +6,30 @@ from collections import deque
 class TekoalyParannettu(Tekoaly):
     def __init__(self, muistin_koko: int) -> None:
         self._muisti = deque(maxlen=muistin_koko)
-        self.__muistin_koko: int = muistin_koko
-        self._vapaa_muisti_indeksi = 0
-        self.__frekvenssit: dict[str, int] = {"k": 0, "p": 0, "s": 0}
-        self.__voittavat_siirrot: dict[str, str] = {"k": "p", "p": "s", "s": "k"}
+        self.__haviavat_siirrot: dict[str, str] = {"k": "p", "p": "s", "s": "k"}
 
     def aseta_siirto(self, siirto: str) -> None:
-        if len(self._muisti) >= self.__muistin_koko:
-            poistettava = self._muisti[-1]
-            self.__frekvenssit[poistettava] -= 1
-
-        poistettava = self._muisti[-1]
-
-        self.__frekvenssit[siirto] += 1
-        self.__frekvenssit[poistettava] -= 1
-
         self._muisti.append(siirto)
 
-    def anna_siirto(self):
-        if self._vapaa_muisti_indeksi == 0 or self._vapaa_muisti_indeksi == 1:
+    def anna_siirto(self) -> str:
+        if len(self._muisti) <= 2:
             return "k"
 
-        viimeisin_siirto = self._muisti[self._vapaa_muisti_indeksi - 1]
+        yleisin_siirto = self.__hae_yleisin_siirto(self._muisti[-1])
 
-        k = 0
-        p = 0
-        s = 0
+        return self.__haviavat_siirrot[yleisin_siirto]
 
-        for i in range(0, self._vapaa_muisti_indeksi - 1):
-            if viimeisin_siirto == self._muisti[i]:
-                seuraava = self._muisti[i + 1]
+    def __hae_yleisin_siirto(self, viimeisin_siirto: str) -> str:
+        frekvenssit = {"k": 0, "p": 0, "s": 0}
+        edellinen = self._muisti[0]
 
-                if seuraava == "k":
-                    k = k + 1
-                elif seuraava == "p":
-                    p = p + 1
-                else:
-                    s = s + 1
+        for i, siirto in enumerate(self._muisti):
+            if i == 0:
+                continue
 
-        # Tehdään siirron valinta esimerkiksi seuraavasti;
-        # - jos kiviä eniten, annetaan aina paperi
-        # - jos papereita eniten, annetaan aina sakset
-        # muulloin annetaan aina kivi
-        if k > p or k > s:
-            return "p"
-        elif p > k or p > s:
-            return "s"
-        else:
-            return "k"
+            if edellinen == viimeisin_siirto:
+                frekvenssit[siirto] += 1
 
-        # Tehokkaampiakin tapoja löytyy, mutta niistä lisää
-        # Johdatus Tekoälyyn kurssilla!
+            edellinen = siirto
+
+        return max(frekvenssit.keys(), key=lambda x: frekvenssit[x])
